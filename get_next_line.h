6efa5bf6e7e925cafc09c9a65538f3a30ca6297d
shinji-japaaaan <84433138+shinji-japaaaan@users.noreply.github.com>
2025-04-12 19:17:25 +0900

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,17 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# ifndef FD_MAX
+#  define FD_MAX 4096
+# endif
+
+#endif
